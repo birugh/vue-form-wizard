4a322f27950ee1f736a164055a240eb54349a9ae
birugh <notblue.https@gmail.com>
2025-12-16 11:06:27 +0700

    add: step 3 & step 4 logic

--- src/views/onboarding/Step3Access.vue
+++ src/views/onboarding/Step3Access.vue
@@ -10,38 +10,38 @@ import FileUpload from 'primevue/fileupload'
 import { Message } from 'primevue'
 
 const schema = yup.object({
-    communication_tools: yup
-        .array()
-        .of(yup.string())
-        .min(1, 'Please select at least one communication tool'),
+  communication_tools: yup
+    .array()
+    .of(yup.string())
+    .min(1, 'Please select at least one communication tool'),
 
-    technical_tools: yup
-        .array()
-        .of(yup.string())
-        .min(1, 'Please select at least one technical tool'),
+  technical_tools: yup
+    .array()
+    .of(yup.string())
+    .min(1, 'Please select at least one technical tool'),
 
-    access_level: yup
-        .string()
-        .required('Access level is required'),
+  access_level: yup
+    .string()
+    .required('Access level is required'),
 
-    specific_zones: yup
-        .array()
-        .of(yup.string())
-        .min(1, 'Please select at least one zone'),
+  specific_zones: yup
+    .array()
+    .of(yup.string())
+    .min(1, 'Please select at least one zone'),
 
-    evidences: yup
-        .array()
-        .min(1, 'Please upload at least one file'),
+  evidences: yup
+    .array()
+    .min(1, 'Please upload at least one file'),
 })
 
 const {
-    validate,
-    setValues,
-    values,
-    errors,
+  validate,
+  setValues,
+  values,
+  errors,
 } = useForm({
-    validationSchema: schema,
-    validateOnMount: false,
+  validationSchema: schema,
+  validateOnMount: false,
 })
 
 const { value: communication_tools } = useField('communication_tools', undefined, { initialValue: [] })
@@ -49,6 +49,8 @@ const { value: technical_tools } = useField('technical_tools', undefined, { init
 const { value: access_level } = useField('access_level')
 const { value: specific_zones } = useField('specific_zones', undefined, { initialValue: [] })
 const { value: evidences } = useField('evidences', undefined, { initialValue: [] })
+console.log(evidences.value);
+
 
 const store = useOnboardingStore()
 
@@ -57,123 +59,125 @@ const zoneOptions = ['Office', 'Server Room', 'Warehouse', 'Data Center', 'Meeti
 const communicationOptions = ['Slack', 'Email', 'Microsoft Teams', 'Zoom', 'WhatsApp']
 const technicalOptions = ['GitHub', 'GitLab', 'Bitbucket', 'Postman', 'Jira', 'Docker', 'Steam']
 const accessLevelOptions = [
-    { label: 'Low', value: 'Low' },
-    { label: 'Medium', value: 'Medium' },
-    { label: 'High', value: 'High' },
+  { label: 'Low', value: 'Low' },
+  { label: 'Medium', value: 'Medium' },
+  { label: 'High', value: 'High' },
 ]
 
 
 const onFileSelect = (event) => {
-    evidences.value = event.files
+  console.log(evidences.value);
+  console.log(event.files);
+
+  evidences.value = event.files
 }
 
 const onFileRemove = (event) => {
-    evidences.value = evidences.value.filter(
-        file => !event.files.some(f => f.name === file.name)
-    )
+  evidences.value = evidences.value.filter(
+    file => !event.files.some(f => f.name === file.name)
+  )
 }
 
 onMounted(() => {
-    if (!store.onboarding?.access_rights) return
-    setValues(store.onboarding.access_rights)
+  if (!store.onboarding?.access_rights) return
+  setValues(store.onboarding.access_rights)
 })
 
 const buildPayload = () => {
-    const formData = new FormData()
+  const formData = new FormData()
 
-    values.communication_tools.forEach(v =>
-        formData.append('communication_tools[]', v)
-    )
+  values.communication_tools.forEach(v =>
+    formData.append('communication_tools[]', v)
+  )
 
-    values.technical_tools.forEach(v =>
-        formData.append('technical_tools[]', v)
-    )
+  values.technical_tools.forEach(v =>
+    formData.append('technical_tools[]', v)
+  )
 
-    formData.append('access_level', values.access_level)
+  formData.append('access_level', values.access_level)
 
-    values.specific_zones.forEach(v =>
-        formData.append('specific_zones[]', v)
-    )
+  values.specific_zones.forEach(v =>
+    formData.append('specific_zones[]', v)
+  )
 
-    evidences.value.forEach(file =>
-        formData.append('evidences[]', file)
-    )
+  evidences.value.forEach(file =>
+    formData.append('evidences[]', file)
+  )
 
-    return formData
+  return formData
 }
 
 defineExpose({
-    async validateStep() {
-        const result = await validate()
-        return result.valid
-    },
-    getPayload() {
-        return buildPayload()
-    },
-    hasValue() {
-        return Object.values(values).some(
-            (v) => v !== null && v !== '' && v !== undefined
-        )
-    },
+  async validateStep() {
+    const result = await validate()
+    return result.valid
+  },
+  getPayload() {
+    return buildPayload()
+  },
+  hasValue() {
+    return Object.values(values).some(
+      (v) => v !== null && v !== '' && v !== undefined
+    )
+  },
 })
 </script>
 
 <template>
-    <section>
+  <section>
 
-        <h2 class="text-2xl font-medium mb-4">Access Rights & Evidence</h2>
-        <div class="h-separator"></div>
+    <h2 class="text-2xl font-medium mb-4">Access Rights & Evidence</h2>
+    <div class="h-separator"></div>
 
-        <form class="flex justify-center flex-col gap-4">
-            <div class="flex justify-center flex-col gap-2 mb-2">
-                <h3 class="text-xl font-medium">System Access</h3>
-                <div class="field-row">
-                    <div class="flex flex-col gap-1 w-80">
-                        <label class="label-field req">Communication Tools</label>
-                        <MultiSelect v-model="communication_tools" :options="communicationOptions" display="chip"
-                            placeholder="Select tools" />
-                        <Message v-if="errors.communication_tools" class="error-messsage" severity="error" size="small"
-                            variant="simple">{{ errors.communication_tools }}</Message>
-                    </div>
-                    <div class="flex flex-col gap-1 w-80">
+    <form class="flex justify-center flex-col gap-4">
+      <div class="flex justify-center flex-col gap-2 mb-2">
+        <h3 class="text-xl font-medium">System Access</h3>
+        <div class="field-row">
+          <div class="flex flex-col gap-1 w-80">
+            <label class="label-field req">Communication Tools</label>
+            <MultiSelect v-model="communication_tools" :options="communicationOptions" display="chip"
+              placeholder="Select tools" />
+            <Message v-if="errors.communication_tools" class="error-messsage" severity="error" size="small"
+              variant="simple">{{ errors.communication_tools }}</Message>
+          </div>
+          <div class="flex flex-col gap-1 w-80">
 
-                        <label class="label-field req">Technical Tools</label>
-                        <MultiSelect v-model="technical_tools" :options="technicalOptions" display="chip"
-                            placeholder="Select tools" />
-                        <Message v-if="errors.technical_tools" class="error-messsage" severity="error" size="small"
-                            variant="simple">{{ errors.technical_tools }}</Message>
-                    </div>
-                </div>
-            </div>
+            <label class="label-field req">Technical Tools</label>
+            <MultiSelect v-model="technical_tools" :options="technicalOptions" display="chip"
+              placeholder="Select tools" />
+            <Message v-if="errors.technical_tools" class="error-messsage" severity="error" size="small"
+              variant="simple">{{ errors.technical_tools }}</Message>
+          </div>
+        </div>
+      </div>
 
-            <div class="flex justify-center flex-col gap-2">
-                <h3 class="text-xl font-medium">Facility Access</h3>
-                <div class="field-row">
-                    <div class="flex flex-col gap-1 w-50">
-                        <label class="label-field req">Access Level</label>
-                        <Select v-model="access_level" :options="accessLevelOptions" optionLabel="label"
-                            optionValue="value" placeholder="Select access level" />
-                        <Message v-if="errors.access_level" class="error-messsage" severity="error" size="small"
-                            variant="simple">{{ errors.access_level }}</Message>
-                    </div>
-                    <div class="flex flex-col gap-1 w-80">
+      <div class="flex justify-center flex-col gap-2">
+        <h3 class="text-xl font-medium">Facility Access</h3>
+        <div class="field-row">
+          <div class="flex flex-col gap-1 w-50">
+            <label class="label-field req">Access Level</label>
+            <Select v-model="access_level" :options="accessLevelOptions" optionLabel="label" optionValue="value"
+              placeholder="Select access level" />
+            <Message v-if="errors.access_level" class="error-messsage" severity="error" size="small" variant="simple">{{
+              errors.access_level }}</Message>
+          </div>
+          <div class="flex flex-col gap-1 w-80">
 
-                        <label class="label-field req">Specific Zones</label>
-                        <MultiSelect v-model="specific_zones" :options="zoneOptions" display="chip"
-                            placeholder="Select zones" />
-                        <Message v-if="errors.specific_zones" class="error-messsage" severity="error" size="small"
-                            variant="simple">{{ errors.specific_zones }}</Message>
-                    </div>
-                </div>
-            </div>
+            <label class="label-field req">Specific Zones</label>
+            <MultiSelect v-model="specific_zones" :options="zoneOptions" display="chip" placeholder="Select zones" />
+            <Message v-if="errors.specific_zones" class="error-messsage" severity="error" size="small" variant="simple">
+              {{ errors.specific_zones }}</Message>
+          </div>
+        </div>
+      </div>
 
-            <div class="flex justify-center flex-col gap-4">
-                <label class="label-field req">Upload Evidence</label>
-                <FileUpload mode="advanced" customUpload multiple accept="image/*,application/pdf"
-                    :maxFileSize="5000000" @select="onFileSelect" @remove="onFileRemove" />
-                <Message v-if="errors.evidences" class="error-messsage" severity="error" size="small" variant="simple">
-                    {{ errors.evidences }}</Message>
-            </div>
-        </form>
-    </section>
+      <div class="flex justify-center flex-col gap-4">
+        <label class="label-field req">Upload Evidence</label>
+        <FileUpload mode="advanced" customUpload multiple accept="image/*,application/pdf" :maxFileSize="5000000"
+          @select="onFileSelect" @remove="onFileRemove" />
+        <Message v-if="errors.evidences" class="error-messsage" severity="error" size="small" variant="simple">
+          {{ errors.evidences }}</Message>
+      </div>
+    </form>
+  </section>
 </template>
--- src/views/onboarding/Step4Preview.vue
+++ src/views/onboarding/Step4Preview.vue
@@ -5,35 +5,58 @@ import PreviewPersonal from '@/components/preview/PreviewPersonal.vue';
 import PreviewJob from '@/components/preview/PreviewJob.vue';
 import PreviewAccess from '@/components/preview/PreviewAccess.vue';
 import PreviewEvidence from '@/components/preview/PreviewEvidence.vue';
-import { Message } from 'primevue';
+import { Divider, Message, Skeleton } from 'primevue';
 
 const store = useOnboardingStore()
 
 const onboarding = computed(() => store.onboarding)
 
 defineExpose({
-    confirmFinal() {
-        if (!onboarding.value?.id) return false
-        return true
-    },
+  confirmFinal() {
+    if (!onboarding.value?.id) return false
+    return true
+  },
 })
 </script>
 
 <template>
-    <section>
-        <h2 class="text-2xl font-medium mb-4">Preview & Confirmation</h2>
-        <Message severity="error">Please review all information before submitting.</Message>
-        <div class="h-separator"></div>
+  <section>
+    <h2 class="text-2xl font-medium mb-4">Preview & Confirmation</h2>
+    <Message severity="error">Please review all information before submitting.</Message>
+    <div class="h-separator"></div>
 
-        <div v-if="!onboarding">
-            <Skeleton width="100%" height="240px"></Skeleton>
-        </div>
+    <div v-if="!onboarding">
+      <div class="flex flex-col gap-4">
+        <Skeleton height="2rem" width="40%" />
+        <Skeleton height="1.5rem" width="30%" />
 
-        <div class="flex flex-col gap-4" v-else>
-            <PreviewPersonal :data="onboarding.personal_information" />
-            <PreviewJob :data="onboarding.job_details" />
-            <PreviewAccess :data="onboarding.access_rights" />
-            <PreviewEvidence :files="onboarding.access_rights?.evidences || []" />
-        </div>
-    </section>
+        <Divider />
+
+        <Skeleton height="1.5rem" width="50%" />
+        <Skeleton height="3rem" width="45%" />
+
+        <Divider />
+
+        <Skeleton height="1.5rem" width="50%" />
+        <Skeleton height="3rem" width="45%" />
+
+        <Divider />
+
+        <Skeleton height="1.5rem" width="50%" />
+        <Skeleton height="3rem" width="45%" />
+
+        <Divider />
+
+        <Skeleton height="1.5rem" width="50%" />
+        <Skeleton height="3rem" width="45%" />
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4" v-else>
+      <PreviewPersonal :data="onboarding.personal_information" />
+      <PreviewJob :data="onboarding.job_details" />
+      <PreviewAccess :data="onboarding.access_rights" />
+      <PreviewEvidence :files="onboarding.access_rights?.evidences || []" />
+    </div>
+  </section>
 </template>
